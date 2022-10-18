ea4f22b21957333dd360f0124ea6c0ddc34e356c
sagi-brudni <brudnis@post.bgu.ac.il>
2022-10-18 18:48:06 +0300

    organize code

--- running anti virus scan.py
+++ running anti virus scan.py
@@ -1,23 +1,22 @@
 import psutil
-import time
-import numpy as np
-from subprocess import call
-
 import pythoncom
 import wmi
 from prettytable import PrettyTable
-from datetime import datetime
-import matplotlib.pyplot as plt
 import subprocess
-from multiprocessing import Process
 from threading import Thread
 import time
 import pandas as pd
 
+PROCESSES_CSV = 'processes_data.csv'
+TOTAL_MEMORY_EACH_MOMENT_CSV = 'total_memory_each_moment.csv'
+DISK_IO_EACH_MOMENT = 'disk_io_each_moment.csv'
+BATTERY_STATUS_CSV = 'battery_status.csv'
+GENERAL_INFORMATION_FILE = 'general_information.txt'
 
 ANTIVIRUS_PROCESS_NAME = "MsMpeng"
 SYSTEM_IDLE_PROCESS_NAME = "System Idle Process"
 SYSTEM_IDLE_PID = 0
+
 GB = 2**30
 MB = 2**20
 KB = 2**10
@@ -50,48 +49,18 @@ def calc_time_interval():
     return time.time() - starting_time
 
 
-# [ [x1, y1] [x2, y2] [x3,y3]] to [x1 x2 x3] and [y1 y2 y3]
-def split_to_xy(arr):
-    print("This is split")
-    print(arr)
-    x, y = zip(*arr)
-    return x, y
-
-
-def draw_graph(x, y, y_name):
-    # plotting the points
-    plt.plot(x, y)
-
-    # naming the x axis
-    plt.xlabel('Time')
-    # naming the y axis
-    plt.ylabel(y_name)
-
-    # giving a title to my graph
-    plt.title('My first graph!')
-
-    # function to show the plot
-    plt.show()
-
-
 def save_battery_stat():
-    # if mobile computer
     # Fetch the battery information
     battery = psutil.sensors_battery()
     if battery is None:  # if desktop computer (has no battery)
         return
 
-    t = wmi.WMI(moniker = "//./root/wmi")
+    if battery.power_plugged:
+        raise Exception("Unplug charging cable during measurements!")
+
+    t = wmi.WMI(moniker="//./root/wmi")
 
     new_row_index = len(battery_df.index)
-
-    """ batts1 = c.CIM_Battery(Caption='Portable Battery')
-    for i, b in enumerate(batts1):
-        print('Battery %d Design Capacity: %d mWh' % (i, b.DesignCapacity or 0))
-
-    batts = t.ExecQuery('Select * from BatteryFullChargedCapacity')
-    for i, b in enumerate(batts):
-        print('Battery %d Fully Charged Capacity: %d mWh' % (i, b.FullChargedCapacity))"""
 
     for i, b in enumerate(t.ExecQuery('Select * from BatteryStatus where Voltage > 0')):
         battery_df.loc[new_row_index + i] = [
@@ -101,11 +70,8 @@ def save_battery_stat():
             b.Voltage
         ]
 
-    if battery.power_plugged:
-        raise Exception("Unplug charging cable during measurements!")
 
-
-def create_total_memory_table():
+def save_total_memory():
     vm = psutil.virtual_memory()
     memory_df.loc[len(memory_df.index)] = [
         calc_time_interval(),
@@ -116,22 +82,7 @@ def create_total_memory_table():
     ]
 
 
-def create_total_disk_table():
-    print("----Disk----")
-    disk_table = PrettyTable(["Total(GB)", "Used(GB)",
-                              "Available(GB)", "Percentage"])
-    disk_stat = psutil.disk_usage('/')
-    disk_table.add_row([
-        f'{disk_stat.total / GB:.3f}',
-        f'{disk_stat.used / GB:.3f}',
-        f'{disk_stat.free / GB:.3f}',
-        disk_stat.percent
-    ])
-    # used_total_disk.append([calc_time_interval(), f'{disk_stat.used / GB:.3f}'])
-    print(disk_table)
-
-
-def create_current_disk_io_table(previous_disk_io):
+def save_current_disk_io(previous_disk_io):
     disk_io_stat = psutil.disk_io_counters()
     disk_io_each_moment_df.loc[len(disk_io_each_moment_df.index)] = [
         calc_time_interval(),
@@ -144,7 +95,7 @@ def create_current_disk_io_table(previous_disk_io):
     return disk_io_stat
 
 
-def create_process_table():
+def save_processes_statistics():
     proc = []
     system_idle_process = psutil.Process(SYSTEM_IDLE_PID)
     system_idle_process.cpu_percent()
@@ -184,11 +135,6 @@ def add_to_processes_dataframe(time_of_sample, top_list):
 
         # While fetching the processes, some subprocesses may exit
         # Hence we need to put this code in try-except block
-        # TODO: CHANGE TO DATAFRAME
-        """if p.name() == SYSTEM_IDLE_PROCESS_NAME:
-            used_total_cpu.append([calc_time_interval(), float(f'{100 - cpu_percent:.2f}')])
-        elif need_scan and p.name() == ANTIVIRUS_PROCESS_NAME:
-            used_cpu_by_antivirus.append([calc_time_interval(), f'{cpu_percent:.2f}'])"""
         try:
             # oneshot to improve info retrieve efficiency
             with p.oneshot():
@@ -223,9 +169,9 @@ def continuously_measure():
     # TODO: think if total tables should be printed only once
     while not isScanDone if need_scan else (SCAN_TIME + starting_time >= time.time()):
         save_battery_stat()
-        create_process_table()
-        create_total_memory_table()
-        prev_disk_io = create_current_disk_io_table(prev_disk_io)
+        save_processes_statistics()
+        save_total_memory()
+        prev_disk_io = save_current_disk_io(prev_disk_io)
 
         # Create a delay
         time.sleep(0.5)
@@ -255,27 +201,41 @@ def save_general_disk(f):
         f'{disk_stat.free / GB:.3f}',
         disk_stat.percent
     ])
-    # used_total_disk.append([calc_time_interval(), f'{disk_stat.used / GB:.3f}'])
     f.write(str(disk_table))
     f.write('\n')
 
 
-def save_general_information():
-    with open("general_information.txt", 'w') as f:
+def save_general_information_before_scanning():
+    with open(GENERAL_INFORMATION_FILE, 'w') as f:
+        f.write('======Before Scanning======\n')
         save_general_battery(f)
         f.write('\n')
         save_general_disk(f)
+        f.write('\n\n')
+
+
+def save_general_information_after_scanning():
+    with open(GENERAL_INFORMATION_FILE, 'a') as f:
+        f.write('======After Scanning======\n')
+        save_general_disk(f)
+
+
+def save_to_files():
+    save_general_information_after_scanning()
+    processes_df.to_csv(PROCESSES_CSV)
+    memory_df.to_csv(TOTAL_MEMORY_EACH_MOMENT_CSV)
+    disk_io_each_moment_df.to_csv(DISK_IO_EACH_MOMENT)
+    battery_df.to_csv(BATTERY_STATUS_CSV)
 
 
 def main():
     global isScanDone
-    print("==============================Process Monitor\
-                ======================================")
-    # print_battery_stat()
-    save_general_information()
+    print("======== Process Monitor ========")
 
-    mainT = Thread(target=continuously_measure, args=())
-    mainT.start()
+    save_general_information_before_scanning()
+
+    measurements_thread = Thread(target=continuously_measure, args=())
+    measurements_thread.start()
 
     if need_scan:
         # TODO check about capture_output
@@ -284,27 +244,11 @@ def main():
         if result.returncode != 0:
             raise Exception("An error occurred while anti virus scan: %s", result.stderr)
 
-    mainT.join()
+    measurements_thread.join()
 
-    # print_battery_stat()
-    # create_total_disk_table()
-
-    processes_df.to_csv('processes_data.csv')
-    memory_df.to_csv('total_memory.csv')
-    disk_io_each_moment_df.to_csv('disk_io_each_moment.csv')
-    battery_df.to_csv('battery_status.csv')
+    save_to_files()
 
     print("finished scanning")
-
-    """print("done waiting")
-    print("Time: " + str(time.time() - starting_time))
-    print("memory data: ")
-    print(used_total_memory)
-    x, y = split_to_xy(used_total_memory)
-    draw_graph(x, y, "used memory")
-    x, y = split_to_xy(used_total_cpu)
-    draw_graph(x, y, "used cpu")
-"""
 
 
 if __name__ == '__main__':
