4c4dd452e739872b624fdfd017642be3bbc060f6
Sapir <sapirani@post.bgu.ac.il>
2022-11-10 18:10:12 +0200

    change configuration

--- program_parameters.py
+++ program_parameters.py
@@ -4,14 +4,14 @@ from general_consts import *
 scan_command = ScanCommand.antivirus
 
 power_plan = PowerPlan.BALANCED
-scan_option = ScanMode.ONE_SCAN
+scan_option = ScanMode.NO_SCAN
 scan_type = ScanType.QUICK_SCAN  # relevant only for one scan or continuous scan
 custom_scan_path = r""  # relevant only for custom scans. On other types, must be empty
 
 MINIMUM_DELTA_CAPACITY = 20
 MINIMUM_SCAN_TIME = 0.5 * MINUTE
 
-measurement_number = 8000    # write number between 1->inf or type NEW_MEASUREMENT
+measurement_number = NEW_MEASUREMENT    # write number between 1->inf or type NEW_MEASUREMENT
 
 # MUST disable tamper protection manually for this feature to work
 disable_real_time_protection_during_measurement = False  # must use administrator permissions
