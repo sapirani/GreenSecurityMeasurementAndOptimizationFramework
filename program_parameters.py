4803fc95ae49b3f9fb1aeb3c18c3029fb0be54ab
sagi-brudni <brudnis@post.bgu.ac.il>
2022-11-24 00:23:46 +0200

    program parameters

--- program_parameters.py
+++ program_parameters.py
@@ -1,23 +1,28 @@
 from general_consts import *
 
 # ======= Scanner Parameters =======
-scan_command = ScanCommand.antivirus
+program_to_scan = ProgramToScan.NO_SCAN
 
 power_plan = PowerPlan.BALANCED
 scan_option = ScanMode.ONE_SCAN
 scan_type = ScanType.QUICK_SCAN  # relevant only for one scan or continuous scan
-custom_scan_path = r""  # relevant only for custom scans. On other types, must be empty
+custom_scan_path = r'""'  # relevant only for custom scans. On other types, must be empty
 
 MINIMUM_DELTA_CAPACITY = 20
 MINIMUM_SCAN_TIME = 0.5 * MINUTE
 
-measurement_number = NEW_MEASUREMENT    # write number between 1->inf or type NEW_MEASUREMENT
+measurement_number = 8000    # write number between 1->inf or type NEW_MEASUREMENT
 
 # MUST disable tamper protection manually for this feature to work
-disable_real_time_protection_during_measurement = True  # must use administrator permissions
+disable_real_time_protection_during_measurement = False  # must use administrator permissions
 
 screen_brightness_level = 75    # A number between 0 and 100
 
 # return to default settings (can be costumed)
 DEFAULT_SCREEN_TURNS_OFF_TIME = 4
 DEFAULT_TIME_BEFORE_SLEEP_MODE = 4
+
+
+ids_type = IdsType.SURICATA
+interface_name = "132.73.204.238"
+log_dir = "LogFile"
