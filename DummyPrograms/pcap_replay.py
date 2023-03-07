da52cf13d9f6662f57b57db5fa2be5f5e1bd0230
sagi-brudni <brudnis@post.bgu.ac.il>
2023-03-07 19:43:31 +0200

    improve timing pcap replay

--- DummyPrograms/pcap_replay.py
+++ DummyPrograms/pcap_replay.py
@@ -1,39 +1,50 @@
 from scapy.all import *
 import time
 import sys
+import platform
 
 INTERFACE_NAME = "wlp0s20f3"
 
-res = subprocess.run(f'cat /sys/class/net/{INTERFACE_NAME}/mtu', capture_output=True, shell=True)
-if res.returncode != 0:
-    raise Exception("cannot get the value of MTU", res.stderr)
+if platform.system() == "Linux":
+    res = subprocess.run(f'cat /sys/class/net/{INTERFACE_NAME}/mtu', capture_output=True, shell=True)
+    if res.returncode != 0:
+        raise Exception("cannot get the value of MTU", res.stderr)
 
-mtu = int(res.stdout.decode("utf-8"))
-#mtu=1500
-clk = None
+    mtu = int(res.stdout.decode("utf-8"))
 
+else:
+    mtu = 1500
+
+
+first_time = None
+first_pcap_time = None
 packet_counter = 0
 
 
 def send_packets(p):
     global packet_counter
-    global clk
-    #print(p, packet_counter)
+    global first_time
+    global first_pcap_time
+
     if len(p) <= mtu:
         next_time = float(p.time)
 
-        if packet_counter > 0:
-            time.sleep(next_time - clk)
-        sendp(p, verbose=False)
-        clk = next_time
+        if first_time is None:
+            first_time = time.time()
+            first_pcap_time = next_time
+            print("start")
 
-        if (packet_counter + 1) % 500 == 0:
+        wait_time = (next_time - first_pcap_time) - (time.time() - first_time)
+        if wait_time > 0:
+            time.sleep(wait_time)
+        sendp(p, verbose=False)
+
+        if (packet_counter + 1) % 10 == 0:
             print(f"sent {packet_counter + 1} packets (in total)")
 
     else:
         print(f"message is too long: {len(p)} bytes. Index: {packet_counter}")
     packet_counter += 1
-
 
 
 sniff(offline=sys.argv[1], prn=send_packets)
