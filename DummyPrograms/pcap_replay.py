61febcce187385345b97b52f6f228e5eab67e93e
sagi-brudni <brudnis@post.bgu.ac.il>
2023-03-07 14:06:20 +0200

    implement pcap replay

---
+++ DummyPrograms/pcap_replay.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+import time
+import sys
+
+packets = rdpcap(sys.argv[1])
+clk = float(packets[0].time)
+for index, p in enumerate(packets):
+    next_time = float(p.time)
+    time.sleep(next_time - clk)
+    clk = next_time
+    sendp(p, verbose=False)
+
+    if (index + 1) % 40 == 0:
+        print(f"sent {index + 1} packets (in total)")
+
