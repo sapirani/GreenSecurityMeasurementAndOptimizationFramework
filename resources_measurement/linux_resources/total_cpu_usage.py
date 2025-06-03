83eb128ab864241814d76d840702ade8389acc27
Sapir <sapirani@post.bgu.ac.il>
2025-06-03 18:35:44 +0300

    use real cpus file to check how many cpu's

--- resources_measurement/linux_resources/total_cpu_usage.py
+++ resources_measurement/linux_resources/total_cpu_usage.py
@@ -6,14 +6,14 @@ DEFAULT_NUMBER_OF_CPUS = 1
 
 # A cgroup is a feature that allows you to allocate, limit, and monitor system resources among user-defined groups of processes.
 # Enables control over resource distribution, ensuring that no single group can monopolize the system resources.
-SYS_FS_CGROUP_PATH = r"/sys/fs/cgroup/"
+SYSTEM_CGROUP_FILE_PATH = r"/sys/fs/cgroup/"
 
 # Lists the available controllers (e.g., cpu, memory) that can be enabled in the current cgroup.
 # The format of the file is a single line with space-separated controller names.
 # E.g. cpu io memory
 # Presence of this file indicates that the system is using cgroup v2.
 CGROUP_CONTROLLERS_FILE_NAME = r"cgroup.controllers"
-CGROUP_CONTROLLERS_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CGROUP_CONTROLLERS_FILE_NAME)
+CGROUP_CONTROLLERS_FILE_PATH = os.path.join(SYSTEM_CGROUP_FILE_PATH, CGROUP_CONTROLLERS_FILE_NAME)
 
 # Provides CPU usage statistics for the cgroup.
 # The format of the file is key-value pairs, one per line.
@@ -23,14 +23,10 @@ CGROUP_CONTROLLERS_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CGROUP_CONTROLLE
 #   system_usec 11111111
 # The interesting key is usage_usec, its value is the total CPU time consumed by all tasks in the cgroup, in microseconds.
 CPU_STATS_FILE_NAME = r"cpu.stat"
-CPU_STATS_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_STATS_FILE_NAME)
 
 # Reports the total CPU time consumed by tasks in the cgroup. - relevant for cgroup V1
 # The format of the file is a single integer value representing nanoseconds.
 CPU_ACCT_USAGE_FILE_NAME = r"cpuacct.usage"
-
-#CPU_ACCT_USAGE_FILE_NAME = r"cpuacct/cpuacct.usage"
-CPU_ACCT_USAGE_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_ACCT_USAGE_FILE_NAME)
 
 # Sets CPU usage limits for the cgroup. - relevant for cgroup V2
 # The format of the file is two values separated by a space: <max> <period>
@@ -39,19 +35,19 @@ CPU_ACCT_USAGE_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_ACCT_USAGE_FILE_
 # If <max> is set to max, there is no CPU limit.
 # E.g. 50000 100000 - means that we limit the cgroup to 50ms of CPU time every 100ms.
 CPU_MAX_FILE_NAME = r"cpu.max"
-CPU_MAX_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_MAX_FILE_NAME)
+CPU_MAX_FILE_PATH = os.path.join(SYSTEM_CGROUP_FILE_PATH, CPU_MAX_FILE_NAME)
 
 # Specifies the total available run-time within a period for tasks in the cgroup. - relevant for cgroup V1
 # A quota sets the maximum amount of CPU time that a cgroup can consume during each period.
 # The format of the file is a single integer value in microseconds (-1 indicates no limit).
-CPU_CF_QUOTA_FILE_NAME = r"cpu/cpu.cfs_quota_us"
-CPU_CF_QUOTA_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_CF_QUOTA_FILE_NAME)
+CPU_CFS_QUOTA_FILE_NAME = r"cpu/cpu.cfs_quota_us"
+CPU_CFS_QUOTA_FILE_PATH = os.path.join(SYSTEM_CGROUP_FILE_PATH, CPU_CFS_QUOTA_FILE_NAME)
 
 # Defines the length of the period for enforcing CPU quotas. - relevant for cgroup V1
 # Within each period, the cgroup's CPU usage is limited according to its quota.
 # The format of the file is a single integer value in microseconds.
-CPU_CF_PERIOD_FILE_NAME = r"cpu/cpu.cfs_period_us"
-CPU_CF_PERIOD_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_CF_PERIOD_FILE_NAME)
+CPU_CFS_PERIOD_FILE_NAME = r"cpu/cpu.cfs_period_us"
+CPU_CFS_PERIOD_FILE_PATH = os.path.join(SYSTEM_CGROUP_FILE_PATH, CPU_CFS_PERIOD_FILE_NAME)
 
 
 # Contains details on the cgroup of the container.
@@ -61,7 +57,9 @@ CPU_CF_PERIOD_FILE_PATH = os.path.join(SYS_FS_CGROUP_PATH, CPU_CF_PERIOD_FILE_NA
 # Controller(s)	can be cpu,cpuacct or empty string ("") for v2
 # Path to cgroup can be /docker/<container-id> or /
 CGROUP_IN_CONTAINER_PATH = r"/proc/self/cgroup"
-SYSTEM_CGROUP_FILE_PATH = r"/sys/fs/cgroup/"
+
+CPUSET_CPUS_FILE_NAME = r"cpuset.cpus"
+CPUSET_CPUS_FILE_PATH = os.path.join(SYSTEM_CGROUP_FILE_PATH, CPUSET_CPUS_FILE_NAME)
 
 NEEDED_OPERATIONS = "cpu,cpuacct"
 
@@ -72,15 +70,14 @@ class FileKeywords:
     USAGE_USEC = "usage_usec"
     MAX = "max"
 
+    CGROUP_V1_IDENTIFIER = "cpu,cpuacct"
+    CGROUP_V2_IDENTIFIER = "0"
+
 
 class LinuxContainerCPUReader:
     def __init__(self):
         self.__version = self.__detect_cgroup_version()
-        self._cgroup_path = self.__get_actual_cgroup_cpu_path(self.__version)
-        self._cpu_stat_path = os.path.join(self._cgroup_path, CPU_STATS_FILE_NAME)
-        self._cpuacct_usage_path = os.path.join(self._cgroup_path, CPU_ACCT_USAGE_FILE_NAME)
-
-        # self.__quota_period = self.__read_cpu_limit()
+        self.__cpu_stats_path = self.__get_cpu_file_path(self.__version)
         self.__allowed_cpus = self.__get_num_cpus_allowed()
         self.__last_usage_ns = None
         self.__last_time = None
@@ -115,60 +112,74 @@ class LinuxContainerCPUReader:
 
     def __read_cpu_usage_ns(self) -> Optional[int]:
         if self.__version == FileKeywords.V2:
-            with open(self._cpu_stat_path) as f:
+            with open(self.__cpu_stats_path) as f:
                 for line in f:
                     if line.startswith(FileKeywords.USAGE_USEC):
-                        return int(line.split()[1]) * 1000  # convert to nanoseconds
+                        return int(line.split()[1]) * 1000  # convert to nanoseconds 143512538
         else:
-            with open(self._cpuacct_usage_path) as f:
+            with open(self.__cpu_stats_path) as f:
                 return int(f.read().strip())
 
-    def __detect_cgroup_version(self) -> str:
-        if os.path.exists(CGROUP_CONTROLLERS_FILE_PATH):
-            return FileKeywords.V2
-        else:
-            return FileKeywords.V1
 
-    def __get_actual_cgroup_cpu_path(self, version: str) -> str:
+    def __detect_cgroup_version(self) -> str:
+        return FileKeywords.V2 if os.path.exists(CGROUP_CONTROLLERS_FILE_PATH) else FileKeywords.V1
+
+    def __extract_cgroup_cpu_relative_path(self, version: str) -> str:
         with open(CGROUP_IN_CONTAINER_PATH, "r") as f:
             for line in f:
                 parts = line.strip().split(":")
-                if version == FileKeywords.V2:
-                    if len(parts) == 3 and parts[0] == "0":
-                        return os.path.join(SYSTEM_CGROUP_FILE_PATH, parts[2].lstrip("/"))
-                else:
-                    if len(parts) == 3 and parts[1] == NEEDED_OPERATIONS:
-                        return os.path.join(SYSTEM_CGROUP_FILE_PATH, parts[2].lstrip("/"))
+                if version == FileKeywords.V2 and len(parts) == 3 and parts[0] == FileKeywords.CGROUP_V2_IDENTIFIER:
+                    return os.path.join(SYSTEM_CGROUP_FILE_PATH, parts[2].lstrip("/"))
+                elif version == FileKeywords.V1 and len(parts) == 3 and parts[1] == FileKeywords.CGROUP_V1_IDENTIFIER:
+                    return os.path.join(SYSTEM_CGROUP_FILE_PATH, parts[2].lstrip("/"))
         return SYSTEM_CGROUP_FILE_PATH  # fallback (might be incorrect)
 
+    def __get_cpu_file_path(self, version: str) -> str:
+        path_to_cgroup_dir = self.__extract_cgroup_cpu_relative_path(version)
+        if version == FileKeywords.V2:
+            return os.path.join(path_to_cgroup_dir, CPU_STATS_FILE_NAME)
+        else:
+            return os.path.join(path_to_cgroup_dir, CPU_ACCT_USAGE_FILE_NAME)
 
-    # def __read_cpu_limit(self) -> Optional[tuple[int, int]]:
-    #     if self.__version == FileKeywords.V2:
-    #         try:
+    def __parse_cpuset_cpus(self, cpus_string: str) -> int:
+        # Example format: "0-3,5,7-8"
+        count = 0
+        for part in cpus_string.split(','):
+            if '-' in part:
+                start, end = map(int, part.split('-'))
+                count += end - start + 1
+            else:
+                count += 1
+        return count
+
+
+    def __get_num_cpus_allowed(self) -> int:
+        try:
+            with open(CPUSET_CPUS_FILE_PATH) as f:
+                return self.__parse_cpuset_cpus(f.read().strip())
+        except Exception:
+            return os.cpu_count() or DEFAULT_NUMBER_OF_CPUS
+
+
+
+    # def __get_num_cpus_allowed(self) -> int:
+    #     host_cpu_count = os.cpu_count()
+    #     default_return_value = host_cpu_count if host_cpu_count is not None else DEFAULT_NUMBER_OF_CPUS
+    #     try:
+    #         if self.__version == FileKeywords.V2:
     #             with open(CPU_MAX_FILE_PATH) as f:
     #                 quota_str, period_str = f.read().strip().split()
     #                 if quota_str == FileKeywords.MAX:
-    #                     return None  # no limit
-    #                 return int(quota_str), int(period_str)
-    #         except:
-    #             return None
-    #     else:
-    #         try:
-    #             with open(CPU_CF_QUOTA_FILE_PATH) as f:
+    #                     return default_return_value
+    #                 return int(int(quota_str) / int(period_str))
+    #
+    #         else:
+    #             with open(CPU_CFS_QUOTA_FILE_PATH) as f:
     #                 quota = int(f.read().strip())
-    #             with open(CPU_CF_PERIOD_FILE_PATH) as f:
+    #             with open(CPU_CFS_PERIOD_FILE_PATH) as f:
     #                 period = int(f.read().strip())
     #             if quota == -1:
-    #                 return None
-    #             return quota, period
-    #         except:
-    #             return None
-
-    def __get_num_cpus_allowed(self) -> int:
-        cpu_count = os.cpu_count()
-        return cpu_count if cpu_count is not None else DEFAULT_NUMBER_OF_CPUS
-        # if self.__quota_period is None:
-        #     cpu_count = os.cpu_count()
-        #     return cpu_count if cpu_count is not None else DEFAULT_NUMBER_OF_CPUS
-        # quota, period = self.__quota_period
-        # return int(quota / period)
+    #                 return default_return_value
+    #             return int(quota / period)
+    #     except Exception as e:
+    #         return default_return_value
