b347b26c7b93033d23fa9ee2c14cf60ae3581880
sapir <sapirani@post.bgu.ac.il>
2025-06-16 13:19:43 +0300

    verify add, mul operations

--- tasks/confidential_computing_tasks/homomorphic_encryption_tasks/paillier_encryption/full_pipeline.py
+++ tasks/confidential_computing_tasks/homomorphic_encryption_tasks/paillier_encryption/full_pipeline.py
@@ -8,15 +8,24 @@ if __name__ == '__main__':
     g, n = my_key.public_key
     m1 = 71
     m2 = 29
-
     r1 = paillier_he.get_r_for_encryption(n)
     r2 = paillier_he.get_r_for_encryption(n)
 
-
     c1 = paillier_he.encrypt(m1, my_key.public_key, r1)
     c2 = paillier_he.encrypt(m2, my_key.public_key, r2)
+
+    # validate simple encryption works
+    print(paillier_he.decrypt(c1) == m1)
+
+    # validate that d(e(m1 + m2)) == d(e(m1) * e(m2))
     en_mult = (c1 * c2) % (n * n)
     add_en = paillier_he.encrypt(m1 + m2, my_key.public_key, r1*r2)
 
     print(paillier_he.decrypt(en_mult) == paillier_he.decrypt(add_en))
+
+    # validate that d(e(m1)^m2) == m1m2 mod n
+    en_pow = pow(c1, m2, n * n)
+    mul_en = (m2 * m1) % n
+
+    print(paillier_he.decrypt(en_pow) == mul_en)
     print("Success")
