19b9228e12c60dfff6fee78a0fd895c02b8c3681
sapir <sapirani@post.bgu.ac.il>
2025-06-18 18:23:23 +0300

    add pipeline support for Lightphe library

--- tasks/confidential_computing_tasks/homomorphic_encryption_tasks/algorithms/light_phe_security_algorithm.py
+++ tasks/confidential_computing_tasks/homomorphic_encryption_tasks/algorithms/light_phe_security_algorithm.py
@@ -5,21 +5,33 @@ from tasks.confidential_computing_tasks.homomorphic_encryption_tasks.homomorphic
 from tasks.confidential_computing_tasks.key_details import PRIME_MIN_VAL, PRIME_MAX_VAL, KeyDetails
 
 
+class LightPHEAlgorithms:
+    RSA = "RSA"
+    ELGAMAL = "ElGamal"
+    EXPONENTIAL_ELGAMAL = "Exponential-ElGamal"
+    PAILLIER = "Paillier"
+    DAMGARD_JURIK = "Damgard-Jurik"
+    OKAMOTO_UCHIYAMA = "Okamoto-Uchiyama"
+    BENALOH = "Benaloh"
+    NACCACHE_STERN = "Naccache-Stern"
+    GOLDWASSER_MICALI = "Goldwasser-Micali"
+    ELLIPTICCURVE_ELGAMAL = "EllipticCurve-ElGamal"
+
+
 class LightPHESecurityAlgorithm(HomomorphicSecurityAlgorithm[Ciphertext]):
     def __init__(self, algorithm: str, min_key_val: int = PRIME_MIN_VAL, max_key_val: int = PRIME_MAX_VAL):
         super().__init__(min_key_val, max_key_val)
         self.__algorithm = algorithm
         self.__encryption_model = LightPHE(algorithm_name=algorithm)
 
-
-    def extract_key(self, key_file: str) -> KeyDetails: # todo: maybe extract from the key file the alg name?
+    def extract_key(self, key_file: str) -> KeyDetails:  # todo: maybe extract from the key file the alg name?
         """ Initialize the public and private key """
-        raise NotImplementedError("This method is not implemented for LightPHE library.")
+        print("Key extraction method is not implemented for LightPHE library.")
+        return KeyDetails({}, {})
 
     def encrypt_message(self, msg: int) -> Ciphertext:
         """ Encrypt the message """
         return self.__encryption_model.encrypt(msg)
-
 
     def decrypt_message(self, msg: Ciphertext) -> int:
         """ Decrypt the message """
@@ -83,4 +95,3 @@ if __name__ == '__main__':
     print("d(e(a) * e(b)) == d(e(a*b)): ", dec_mul == regular_mul_dec)
     print("d(e(a) * e(b)) == a * b: ", dec_mul == (a * b))
     print("a * b == d(e(a*b)): ", (a * b) == regular_mul_dec)
-
