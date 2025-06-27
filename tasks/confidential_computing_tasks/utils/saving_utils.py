f794f297909eb5af0617cdf9e1d5aa69864c9670
sapirani <sapirani@post.bgu.ac.il>
2025-06-27 22:19:45 +0300

    fix

--- tasks/confidential_computing_tasks/utils/saving_utils.py
+++ tasks/confidential_computing_tasks/utils/saving_utils.py
@@ -3,9 +3,9 @@ from typing import Any
 
 from tasks.confidential_computing_tasks.action_type import ActionType
 
-LAST_MESSAGE_INDEX_FILE_PATH = r"C:\Users\sapir\Desktop\last_message_index.txt"
+LAST_MESSAGE_INDEX_FILE_PATH = r"C:\Users\Administrator\Desktop\last_message_index.txt"
 
-def get_last_message_index() -> int:
+def get_last_message_index(num_of_messages: int) -> int:
     try:
         with open(LAST_MESSAGE_INDEX_FILE_PATH, "r") as last_message_file:
             content = last_message_file.read().strip()
@@ -13,7 +13,7 @@ def get_last_message_index() -> int:
                 return 0
 
             last_index = int(content)
-            if last_index < 0 or last_index >= len(content):
+            if last_index < 0 or last_index >= num_of_messages:
                 return 0
             
             return last_index
@@ -38,7 +38,7 @@ def extract_messages_from_file(messages_file: str) -> list[int]:
     if len(messages) == 0:
         raise Exception("No messages found. Must be at least one message.")
 
-    last_message_index = get_last_message_index()
+    last_message_index = get_last_message_index(num_of_messages=len(messages))
     return messages[last_message_index:]
 
 
