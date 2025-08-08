68b53d753bfc2a1bfa3d94330624c1ffdaff489c
Sapir <sapirani@post.bgu.ac.il>
2025-08-08 18:17:12 +0300

    memory consumer

--- tasks/resources_consumers/memory_consumer_task.py
+++ tasks/resources_consumers/memory_consumer_task.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
                         help="The speed (bytes per second) of consumption")
 
     parser.add_argument("-c", "--chunk_size",
-                        type=float,
+                        type=int,
                         required=True,
                         help="The chunk size (in bytes) of consumption.")
 
