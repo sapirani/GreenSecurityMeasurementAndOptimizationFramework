11aacf1fd805ca58af54f896853b87d85c9abd0f
Sapir <sapirani@post.bgu.ac.il>
2025-08-24 16:30:26 +0300

    idle extractor

--- aggregative_results/DTOs/aggregators_features/energy_model_features/idle_energy_model_features.py
+++ aggregative_results/DTOs/aggregators_features/energy_model_features/idle_energy_model_features.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class IdleEnergyModelFeatures:
     cpu_usage_idle: float
     memory_usage_idle: float
@@ -7,6 +11,10 @@ class IdleEnergyModelFeatures:
     disk_write_count_usage_idle: float
     disk_read_time_idle_ms_sum: float
     disk_write_time_idle_ms_sum: float
+    network_bytes_kb_sum_sent_idle: float
+    network_packets_sum_sent_idle: int
+    network_bytes_kb_sum_received_idle: float
+    network_packets_sum_received_idle: int
 
     total_energy_consumption_in_idle_mWh: float
     number_of_page_faults_idle: float
--- measurements_model/dataset_creation/data_extractors/idle_extractor.py
+++ measurements_model/dataset_creation/data_extractors/idle_extractor.py
@@ -11,6 +11,20 @@ class IdleExtractor:
         self.__summary_extractor = summary_extractor
 
     def extract(self, idle_summary_file_path: str) -> IdleEnergyModelFeatures:
-        df = pd.read_csv(idle_summary_file_path)
-        idle_summary_results = self.__summary_extractor.extract_system_data(df)
-        return IdleEnergyModelFeatures()
+        idle_summary_results = self.__summary_extractor.extract_system_data(idle_summary_file_path)
+        return IdleEnergyModelFeatures(
+            cpu_usage_idle=idle_summary_results.cpu_usage_system,
+            memory_usage_idle=idle_summary_results.cpu_usage_system,
+            disk_read_bytes_usage_idle=idle_summary_results.disk_read_bytes_kb_usage_system,
+            disk_read_count_usage_idle=idle_summary_results.disk_read_count_usage_system,
+            disk_write_bytes_usage_idle=idle_summary_results.disk_write_bytes_kb_usage_system,
+            disk_write_count_usage_idle=idle_summary_results.disk_write_count_usage_system,
+            disk_read_time_idle_ms_sum=idle_summary_results.disk_read_time_system_ms_sum,
+            disk_write_time_idle_ms_sum=idle_summary_results.disk_write_time_system_ms_sum,
+            network_bytes_kb_sum_sent_idle=idle_summary_results.network_bytes_kb_sum_sent_system,
+            network_packets_sum_sent_idle=idle_summary_results.network_packets_sum_sent_system,
+            network_bytes_kb_sum_received_idle=idle_summary_results.network_bytes_kb_sum_received_system,
+            network_packets_sum_received_idle=idle_summary_results.network_packets_sum_received_system,
+            total_energy_consumption_in_idle_mWh=idle_summary_results.total_energy_consumption_system_mWh,
+            number_of_page_faults_idle=idle_summary_results.number_of_page_faults_system,
+            duration_idle=idle_summary_results.duration_system)
