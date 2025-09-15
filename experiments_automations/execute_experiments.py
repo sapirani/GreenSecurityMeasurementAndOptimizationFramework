a52c65e563ac4c94f0d3767f0dc572644e281bff
Sapir <sapirani@post.bgu.ac.il>
2025-09-15 09:16:15 +0300

    IMPROVE CODE FOR CR

--- experiments_automations/execute_experiments.py
+++ experiments_automations/execute_experiments.py
@@ -2,22 +2,30 @@ import argparse
 import os
 import subprocess
 import sys
+from time import sleep
 
 from human_id import generate_id
 
 DEFAULT_NUMBER_OF_EXPERIMENTS = 3
+SLEEPING_TIME_BETWEEN_MEASUREMENTS = 30
 
-def run_scanner(session_id: str):
-    repo_root = os.path.dirname(os.path.dirname(__file__))
-    scanner_path = os.path.join(repo_root, "scanner.py")
+SCANNER_PROGRAM_FILE = "scanner.py"
+SESSION_ID_SCANNER_FLAG = "--measurement_session_id"
 
-    subprocess.run([sys.executable, scanner_path, "--measurement_session_id", session_id], check=True)
+
+def run_scanner(scanner_path: str, session_id: str):
+    subprocess.run([sys.executable, scanner_path, SESSION_ID_SCANNER_FLAG, session_id], check=True)
 
 
 def automate_experiments(num_of_experiments: int, main_session_id: str):
+    repo_root = os.path.dirname(os.path.dirname(__file__))
+    scanner_path = os.path.join(repo_root, SCANNER_PROGRAM_FILE)
+
     for experiment_id in range(num_of_experiments):
         current_session = f"{main_session_id}_{experiment_id}"
-        run_scanner(current_session)
+        run_scanner(scanner_path, current_session)
+        sleep(SLEEPING_TIME_BETWEEN_MEASUREMENTS)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -29,7 +37,6 @@ if __name__ == '__main__':
                         default=DEFAULT_NUMBER_OF_EXPERIMENTS,
                         help="number of repetitions on the experiment.")
 
-
     parser.add_argument("-s", "--session_id",
                         type=str,
                         default=generate_id(word_count=3),
@@ -37,4 +44,3 @@ if __name__ == '__main__':
 
     arguments = parser.parse_args()
     automate_experiments(arguments.number_of_experiments, arguments.session_id)
-
