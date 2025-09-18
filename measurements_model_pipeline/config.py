8402f0abc75d1b1121008ce3af79c490fec0b1c6
Sapir <sapirani@post.bgu.ac.il>
2025-09-18 09:01:11 +0300

    add energy model file

--- measurements_model_pipeline/config.py
+++ measurements_model_pipeline/config.py
@@ -1,3 +1,5 @@
+import os
+
 SCORING_METHODS_FOR_MODEL = ['neg_mean_absolute_error', 'neg_root_mean_squared_error']
 
-MODEL_FILE_NAME = r"C:\Users\Administrator\Desktop\green security\results\energy_prediction_model.pkl"
+MODEL_FILE_NAME = os.path.join(os.getcwd(), r"measurements_model_pipeline\energy_prediction_model.pkl")
