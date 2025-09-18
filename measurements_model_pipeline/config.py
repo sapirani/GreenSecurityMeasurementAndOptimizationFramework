6539907a44900a3084883d1bdf235b962dd0e394
Sapir <sapirani@post.bgu.ac.il>
2025-09-18 14:48:17 +0300

    change path

--- measurements_model_pipeline/config.py
+++ measurements_model_pipeline/config.py
@@ -1,5 +1,7 @@
 import os
+from pathlib import Path
 
 SCORING_METHODS_FOR_MODEL = ['neg_mean_absolute_error', 'neg_root_mean_squared_error']
 
-MODEL_FILE_NAME = os.path.join(os.getcwd(), r"measurements_model_pipeline\energy_prediction_model.pkl")
+BASE_MODEL_DIR = Path(__file__).parent
+MODEL_FILE_NAME = os.path.join(BASE_MODEL_DIR, r"energy_prediction_model.pkl")
