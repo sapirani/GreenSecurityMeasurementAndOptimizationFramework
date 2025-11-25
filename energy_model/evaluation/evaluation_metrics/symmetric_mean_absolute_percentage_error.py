ef5b2e302b36439b7c9f935cf34399b7a0ccadd4
sapirani <sapirani@post.bgu.ac.il>
2025-11-25 20:24:27 +0200

    add details about the explanations

--- energy_model/evaluation/evaluation_metrics/symmetric_mean_absolute_percentage_error.py
+++ energy_model/evaluation/evaluation_metrics/symmetric_mean_absolute_percentage_error.py
@@ -9,9 +9,17 @@ class SymmetricMeanAbsolutePercentageError(AbstractEvaluationMetric):
     This class implements the symmetric mean absolute percentage error metric.
     Given the actual values y and the predicted values y_hat, the SMAPE is calculated as:
     - the average of the absolute percentage errors between actual values y and the predicted values y_hat
-    - each error is weighted by the sum of the absolute values of the actual and predicted values.
+    - each error is weighted by the magnitude of the prediction and the target combined.
+
+    The denominator is the average of y and y_hat - in order to deal with y ~ 0 and big values of y_hat.
+    why does it work like this?
+        * Using only the true value breaks when y is small or zero
+        * Using only prediction also unfair
+        * Hence, using the average treats both equally and avoids explosions
 
     This metric should better present the errors when the y values are near 0.
+
+    For example, if SMAPE = 20% means that the model’s predictions differ from the true values by 20% on average relative to the magnitude of the prediction and the target combined.
     """
     def get_metric_name(self) -> str:
         return "Symmetric Mean Absolute Percentage Error (Symmetric MAPE)"
