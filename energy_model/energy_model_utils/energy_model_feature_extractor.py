50304c16233b91c5f29a34e6445604102013c719
sapirani <sapirani@post.bgu.ac.il>
2025-11-26 23:29:46 +0200

    handle system energy model features through all aggregator pipeline

--- energy_model/energy_model_utils/energy_model_feature_extractor.py
+++ energy_model/energy_model_utils/energy_model_feature_extractor.py
@@ -2,10 +2,9 @@ from datetime import datetime
 from typing import Optional, Union
 
 from DTOs.aggregators_features.empty_features import EmptyFeatures
-from DTOs.aggregators_features.energy_model_features.full_energy_model_features import EnergyModelFeatures, \
+from DTOs.aggregators_features.energy_model_features.full_energy_model_features import CompleteEnergyModelFeatures, \
     ExtendedEnergyModelFeatures
 from DTOs.aggregators_features.energy_model_features.process_energy_model_features import ProcessEnergyModelFeatures
-from DTOs.aggregators_features.energy_model_features.previous_sample_features import PreviousSampleFeatures
 from DTOs.aggregators_features.energy_model_features.system_energy_model_features import SystemEnergyModelFeatures
 from DTOs.raw_results_dtos.process_raw_results import ProcessRawResults
 from DTOs.raw_results_dtos.system_process_raw_results import ProcessSystemRawResults
@@ -18,15 +17,34 @@ from utils.general_consts import KB
 class EnergyModelFeatureExtractor:
     def __init__(self):
         self.__hardware_details = HardwareExtractor.extract()
-        self.__previous_sample: Optional[PreviousSampleFeatures] = None
+        self.__previous_process_sample: Optional[ProcessRawResults] = None
+        self.__previous_system_sample: Optional[SystemRawResults] = None
+        self.__previous_timestamp: Optional[datetime] = None
 
-    def extract_energy_model_features(self, raw_results: ProcessSystemRawResults,
-                                      timestamp: datetime) -> Union[EnergyModelFeatures, EmptyFeatures]:
+    def extract_system_energy_model_features(self, system_raw_results: SystemRawResults, timestamp: datetime) -> Union[
+        SystemEnergyModelFeatures, EmptyFeatures]:
+        system_features = EmptyFeatures()
+        try:
+            if self.__previous_timestamp is None:
+                return system_features
+
+            duration = (timestamp - self.__previous_timestamp).total_seconds()
+
+            system_features = self.__extract_system_features(
+                system_raw_results,
+                duration
+            )
+        finally:
+            self.__set_previous_sample(None, system_raw_results, timestamp)
+            return system_features
+
+    def extract_process_energy_model_features(self, raw_results: ProcessSystemRawResults,
+                                              timestamp: datetime) -> Union[CompleteEnergyModelFeatures, EmptyFeatures]:
         process_features, system_features, duration = self.__extract_process_system_features(raw_results, timestamp)
         if not process_features or not system_features or not duration:
             return EmptyFeatures()
 
-        return EnergyModelFeatures(
+        return CompleteEnergyModelFeatures(
             duration=duration,
             process_features=process_features,
             system_features=system_features
@@ -61,10 +79,10 @@ class EnergyModelFeatureExtractor:
         system_features = None
         duration = None
         try:
-            if self.__previous_sample is None:
+            if self.__previous_timestamp is None:
                 return process_features, system_features, duration
 
-            duration = (current_timestamp - self.__previous_sample.timestamp).total_seconds()
+            duration = (current_timestamp - self.__previous_timestamp).total_seconds()
             process_features = self.__extract_process_features(
                 raw_results.process_raw_results,
                 duration
@@ -75,28 +93,25 @@ class EnergyModelFeatureExtractor:
                 duration
             )
         finally:
-            self.__set_previous_sample(raw_results, current_timestamp)
+            self.__set_previous_sample(raw_results.process_raw_results, raw_results.system_raw_results,
+                                       current_timestamp)
             return process_features, system_features, duration
 
-    def __set_previous_sample(self, raw_results: ProcessSystemRawResults, timestamp: datetime):
-
-        self.__previous_sample = PreviousSampleFeatures(
-            cpu_percent_sum_across_cores_process=raw_results.process_raw_results.cpu_percent_sum_across_cores,
-            cpu_percent_sum_across_cores_system=raw_results.system_raw_results.cpu_percent_sum_across_cores,
-            memory_mb_usage_process=raw_results.process_raw_results.used_memory_mb,
-            memory_gb_usage_system=raw_results.system_raw_results.total_memory_gb,
-            timestamp=timestamp,
-            battery_remaining_capacity_mWh=raw_results.system_raw_results.battery_remaining_capacity_mWh
-        )
+    def __set_previous_sample(self, process_raw_results: Optional[ProcessRawResults],
+                              system_raw_results: SystemRawResults, timestamp: datetime):
+        self.__previous_timestamp = timestamp
+        self.__previous_process_sample = process_raw_results
+        self.__previous_system_sample = system_raw_results
 
     def __extract_process_features(self, process_data: ProcessRawResults,
                                    duration: float) -> ProcessEnergyModelFeatures:
         process_cpu_time_seconds = EnergyModelFeatureExtractor.__calculate_integral_value(
             process_data.cpu_percent_sum_across_cores,
-            self.__previous_sample.cpu_percent_sum_across_cores_process,
+            self.__previous_process_sample.cpu_percent_sum_across_cores,
             duration) / 100
-        process_memory_relative_usage = process_data.used_memory_mb - self.__previous_sample.memory_mb_usage_process
+        process_memory_relative_usage = process_data.used_memory_mb - self.__previous_process_sample.used_memory_mb
         return ProcessEnergyModelFeatures(
+            duration=duration,
             cpu_usage_seconds_process=process_cpu_time_seconds,
             memory_mb_relative_process=process_memory_relative_usage,
             disk_read_kb_process=process_data.disk_read_kb,
@@ -112,10 +127,12 @@ class EnergyModelFeatureExtractor:
     def __extract_system_features(self, system_data: SystemRawResults, duration: float) -> SystemEnergyModelFeatures:
         system_cpu_time = EnergyModelFeatureExtractor.__calculate_integral_value(
             system_data.cpu_percent_sum_across_cores,
-            self.__previous_sample.cpu_percent_sum_across_cores_system,
+            self.__previous_system_sample.cpu_percent_sum_across_cores,
             duration) / 100
-        system_memory_relative_usage_mb = (system_data.total_memory_gb - self.__previous_sample.memory_gb_usage_system) * KB
+        system_memory_relative_usage_mb = (
+                                                      system_data.total_memory_gb - self.__previous_system_sample.total_memory_gb) * KB
         return SystemEnergyModelFeatures(
+            duration=duration,
             cpu_seconds_system=system_cpu_time,
             memory_mb_relative_system=system_memory_relative_usage_mb,
             disk_read_kb_system=system_data.disk_read_kb,
