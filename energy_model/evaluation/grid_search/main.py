9d7d80fbe0f50c15c915cdbd13e27ff777a1b9a6
sapirani <sapirani@post.bgu.ac.il>
2025-11-26 22:26:15 +0200

    reverse to before dependency injection

--- energy_model/evaluation/grid_search/main.py
+++ energy_model/evaluation/grid_search/main.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from dependency_injector import containers, providers
-from dependency_injector.providers import Provider
 
 from energy_model.configs.columns import ProcessColumns
 from energy_model.energy_model_parameters import PROCESS_SYSTEM_DF_PATH
@@ -8,14 +6,12 @@ from energy_model.evaluation.grid_search.models_config import REGRESSION_MODELS_
 from energy_model.pipelines.grid_search_pipeline_executor import GridSearchPipelineExecutor
 from energy_model.pipelines.pipeline_utils import extract_x_y
 
-class Container(containers.DeclarativeContainer):
-    grid_search_pipeline: Provider[GridSearchPipelineExecutor] = providers.Factory(GridSearchPipelineExecutor, REGRESSION_MODELS_WITH_PARAMETERS)
 
 if __name__ == '__main__':
-    container = Container()
+    grid_search_pipeline = GridSearchPipelineExecutor(REGRESSION_MODELS_WITH_PARAMETERS)
 
     dataset = pd.read_csv(PROCESS_SYSTEM_DF_PATH, index_col=0)
 
     X, y = extract_x_y(dataset, target_column=ProcessColumns.ENERGY_USAGE_PROCESS_COL)
 
-    best_model = container.grid_search_pipeline().run_grid_search(X, y)
+    best_model = grid_search_pipeline.run_grid_search(X, y)
