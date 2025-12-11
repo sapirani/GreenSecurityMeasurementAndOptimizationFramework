c68de12ea4b9baaeb2b1506e8af613f4812e983f
sapirani <sapirani@post.bgu.ac.il>
2025-12-11 18:14:42 +0200

    add choosing task combinations with back processes

--- experiments_automations/execute_parallel_experiments.py
+++ experiments_automations/execute_parallel_experiments.py
@@ -6,13 +6,14 @@ from typing import Optional
 
 from human_id import generate_id
 
+from utils.general_consts import ProgramToScan
 from config import DEFAULT_NUMBER_OF_EXPERIMENTS, SLEEPING_TIME_BETWEEN_MEASUREMENTS, SLEEPING_TIME_BETWEEN_TASKS, \
     SCANNER_PATH, PROGRAM_PARAMETERS_PATH, DEFAULT_MAIN_PROGRAM, DEFAULT_TASKS
 from experiments_automations.run_exeperiment_utils import update_main_program, \
     update_dummy_task_values, run_identical_experiments, update_background_programs
 
 
-def run_various_experiments(num_of_experiments: int, num_of_parallel_tasks: int, main_session_id: str,
+def run_various_experiments(tasks_to_run: list[ProgramToScan], num_of_experiments: int, num_of_parallel_tasks: int, main_session_id: str,
                             rate: Optional[float], size: Optional[int]):
     update_dummy_task_values(PROGRAM_PARAMETERS_PATH, rate=rate, size=size)
     session_id_addition = ""
@@ -21,7 +22,7 @@ def run_various_experiments(num_of_experiments: int, num_of_parallel_tasks: int,
     if rate is not None:
         session_id_addition += f"_{rate}_rate"
 
-    possible_tasks_combinations = list(itertools.combinations(DEFAULT_TASKS, num_of_parallel_tasks))
+    possible_tasks_combinations = list(itertools.combinations(tasks_to_run, num_of_parallel_tasks))
     print("ALL COMBINATIONS:", possible_tasks_combinations)
 
     for task_id, task_combination in enumerate(possible_tasks_combinations):
@@ -71,11 +72,55 @@ if __name__ == '__main__':
                         default=None,
                         help="The number of units per second to perform the default task on.")
 
+    parser.add_argument("--run_memory_consumer",
+                        type=bool,
+                        default=False,
+                        help="Whether to run the memory consumer task.")
+
+    parser.add_argument("--run_memory_releaser",
+                        type=bool,
+                        default=False,
+                        help="Whether to run the memory releaser task.")
+
+    parser.add_argument("--run_disk_writer",
+                        type=bool,
+                        default=False,
+                        help="Whether to run the disk writer task.")
+
+    parser.add_argument("--run_disk_reader",
+                        type=bool,
+                        default=False,
+                        help="Whether to run the disk reader task.")
+
+    parser.add_argument("--run_cpu_consumer",
+                        type=bool,
+                        default=False,
+                        help="Whether to run the cpu consumer task.")
+
     arguments = parser.parse_args()
+    tasks_to_run = []
+    if arguments.run_memory_consumer:
+        tasks_to_run.append(ProgramToScan.MemoryConsumer)
+
+    if arguments.run_memory_releaser:
+        tasks_to_run.append(ProgramToScan.MemoryReleaser)
+
+    if arguments.run_disk_writer:
+        tasks_to_run.append(ProgramToScan.DiskIOWriteConsumer)
+
+    if arguments.run_disk_reader:
+        tasks_to_run.append(ProgramToScan.DiskIOReadConsumer)
+
+    if arguments.run_cpu_consumer:
+        tasks_to_run.append(ProgramToScan.CPUConsumer)
+
+    if len(tasks_to_run) == 0:
+        tasks_to_run = DEFAULT_TASKS
+
     number_of_experiments = arguments.number_of_repetitions_per_experiment
     number_of_parallel_tasks = arguments.number_of_parallel_tasks
     measurement_session_id = arguments.measurement_session_id
 
-    run_various_experiments(number_of_experiments, number_of_parallel_tasks,
+    run_various_experiments(tasks_to_run, number_of_experiments, number_of_parallel_tasks,
                             measurement_session_id, arguments.task_rate,
                             arguments.task_unit_size)
