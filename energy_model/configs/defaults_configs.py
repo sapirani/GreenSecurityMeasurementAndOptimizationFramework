01859f5a5afa415233f253dd847b6c6936984868
sapirani <sapirani@post.bgu.ac.il>
2025-12-28 11:53:11 +0200

    add metrics

--- energy_model/configs/defaults_configs.py
+++ energy_model/configs/defaults_configs.py
@@ -7,20 +7,25 @@ from energy_model.evaluation.evaluation_metrics.mean_squared_error_metric import
 from energy_model.evaluation.evaluation_metrics.percentile_squared_error_metric import PercentileSquaredErrorMetric
 from energy_model.evaluation.evaluation_metrics.relative_root_mean_squared_error_metric import \
     RelativeRootMeanSquaredErrorMetric
+from energy_model.evaluation.evaluation_metrics.rmse_ratio_metric import RootMeanSquaredErrorRatioMetric
 from energy_model.evaluation.evaluation_metrics.root_mean_squared_error_metric import RootMeanSquaredErrorMetric
 from energy_model.evaluation.evaluation_metrics.root_mean_squared_percent_error_metric import \
     RootMeanSquaredPercentErrorMetric
 from energy_model.evaluation.evaluation_metrics.standard_deviation_metric import StandardDeviationMetric
 from energy_model.evaluation.evaluation_metrics.symmetric_mean_absolute_percentage_error import \
     SymmetricMeanAbsolutePercentageError
+from energy_model.evaluation.evaluation_metrics.tail_rmse_percentile_metric import TailRootMeanSquaredErrorMetric
 
 DEFAULT_EVALUATION_METRICS = [SymmetricMeanAbsolutePercentageError(), RelativeRootMeanSquaredErrorMetric(),
                               StandardDeviationMetric(), AveragePERMetric(), MeanSquaredErrorMetric(),
                               RootMeanSquaredErrorMetric(), MeanAbsoluteErrorMetric(),
                               RootMeanSquaredPercentErrorMetric(), PercentileSquaredErrorMetric(90),
-                              PercentileSquaredErrorMetric(95)]
+                              PercentileSquaredErrorMetric(95), TailRootMeanSquaredErrorMetric(),
+                              RootMeanSquaredErrorRatioMetric()]
 
 DEFAULT_BEST_MODEL_METRIC = "Symmetric Mean Absolute Percentage Error (Symmetric MAPE)"
+BEST_SYSTEM_MODEL_METRIC = "Root Mean Squared Error Ratio for Percentile 95"
+BEST_PROCESS_MODEL_METRIC = DEFAULT_BEST_MODEL_METRIC
 
 NON_NEGATIVE_COLUMNS = [SystemColumns.DURATION_COL, SystemColumns.DISK_READ_BYTES_SYSTEM_COL,
                         SystemColumns.DISK_WRITE_BYTES_SYSTEM_COL, SystemColumns.CPU_SYSTEM_COL]
