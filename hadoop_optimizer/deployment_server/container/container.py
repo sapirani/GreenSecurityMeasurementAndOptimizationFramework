6523865c0587c68de5701d9c9306c3c724792967
sagi-brudni <brudnis@post.bgu.ac.il>
2026-01-03 23:30:55 +0200

    Support easy configuration of environment wrappers

--- hadoop_optimizer/deployment_server/container/container.py
+++ hadoop_optimizer/deployment_server/container/container.py
@@ -2,7 +2,6 @@ import gymnasium as gym
 from datetime import datetime
 from dependency_injector import containers, providers
 from dependency_injector.providers import Provider
-from gymnasium.wrappers import OrderEnforcing, FlattenObservation, RescaleObservation, RescaleAction
 from stable_baselines3 import PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.policies import ActorCriticPolicy
@@ -10,12 +9,8 @@ from elastic_reader.consts import TimePickerInputStrategy
 from hadoop_optimizer.deployment_server.drl_manager import DRLManager
 from hadoop_optimizer.drl_telemetry.telemetry_manager import DRLTelemetryManager
 from hadoop_optimizer.drl_envs.deployment_env import OptimizerDeploymentEnv
-from hadoop_optimizer.gymnasium_wrappers.action.action_types_decoder import ActionTypesDecoder
-from hadoop_optimizer.gymnasium_wrappers.action.flatten_action import FlattenAction
-from hadoop_optimizer.gymnasium_wrappers.state.dict_leafs_as_numpy import DictLeafsAsNumpy
-from hadoop_optimizer.gymnasium_wrappers.state.time_limit_wrapper import TimeLimitWrapper
-from hadoop_optimizer.gymnasium_wrappers.state.reset_enforcer import ResetEnforcer
-from hadoop_optimizer.gymnasium_wrappers.state_validators.enforce_observation_bounds import EnforceObservationBounds
+from hadoop_optimizer.env_composition_config.env_builder import build_env
+from hadoop_optimizer.env_composition_config.env_wrapper_spec import EnvWrappersParams
 from user_input.elastic_reader_input.abstract_date_picker import TimePickerChosenInput, ReadingMode
 from user_input.elastic_reader_input.time_picker_input_factory import get_time_picker_input
 
@@ -45,60 +40,15 @@ class Container(containers.DeclarativeContainer):
         telemetry_manager=drl_telemetry_manager
     )
 
-    # TODO: ALLOW EASY CONFIGURATION OF DECORATORS IN BOOTSTRAPPING
-    order_enforcer: Provider[gym.Env] = providers.Factory(
-        OrderEnforcing,
-        base_env,
-    )
-
-    time_limit_env: Provider[gym.Env] = providers.Factory(
-        TimeLimitWrapper,
-        order_enforcer,
-        max_episode_steps=config.max_episode_steps,
-    )
-
-    reset_enforcer_env: Provider[gym.Env] = providers.Factory(
-        ResetEnforcer,
-        time_limit_env,
-    )
-
-    dict_leafs_as_numpy: Provider[gym.Env] = providers.Factory(
-        DictLeafsAsNumpy,
-        reset_enforcer_env,
-    )
-
-    flatten_observation_env: Provider[gym.Env] = providers.Factory(
-        FlattenObservation,
-        dict_leafs_as_numpy,
-    )
-
-    enforce_observation_bounds: Provider[gym.Env] = providers.Factory(
-        EnforceObservationBounds,
-        flatten_observation_env,
-    )
-
-    action_types_decoder_env: Provider[gym.Env] = providers.Factory(
-        ActionTypesDecoder,
-        enforce_observation_bounds,
-    )
-
-    flatten_action_env: Provider[gym.Env] = providers.Factory(
-        FlattenAction,
-        action_types_decoder_env,
-    )
-
-    rescale_action_env: Provider[gym.Env] = providers.Factory(
-        RescaleAction,
-        flatten_action_env,
-        min_action=0,
-        max_action=1,
+    env_wrappers_params: Provider[EnvWrappersParams] = providers.Factory(
+        EnvWrappersParams.from_config,
+        config
     )
 
     deployment_env: Provider[gym.Env] = providers.Factory(
-        RescaleObservation,     # TODO: CONSIDER USING NormalizeObservation
-        rescale_action_env,
-        min_obs=-1,
-        max_obs=1,
+        build_env,
+        base_env=base_env,
+        wrappers_params=env_wrappers_params,
     )
 
     # TODO: LOAD THE BEST AGENT INSTEAD OF INITIALIZING A NEW MODEL HERE, FOR EXAMPLE: PPO.load(<path>)
