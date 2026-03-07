0b7c5b2c5b6e9775203e4c457ffeacdc46eb2065
sapirani <sapirani@post.bgu.ac.il>
2026-03-07 12:42:26 +0200

    revert to main

--- elastic_reader/elastic_consumers/elastic_aggregations_logger.py
+++ elastic_reader/elastic_consumers/elastic_aggregations_logger.py
@@ -23,11 +23,11 @@ class ElasticAggregationsLogger(AbstractElasticConsumer):
             queued_iterations_num_max: int = 15     # used as an optimization for in offline mode
     ):
         self.logger = get_measurement_logger(
-            logger_name=LoggerName.SAPIR_AGGREGATIONS,
+            logger_name=LoggerName.METRICS_AGGREGATIONS,
             logger_handler=get_elastic_bulk_handler(
                 ES_USER,
                 ES_PASS, ES_URL,
-                IndexName.ENERGY_AGGREGATIONS_SAPIR,
+                IndexName.METRICS_AGGREGATIONS,
                 pipeline_name=custom_pipeline_name
             ),
         )
