00e217146ff0f069a0c516b4fc9200b55a038d61
sagi-brudni <brudnis@post.bgu.ac.il>
2026-08-08 23:37:09 +0300

    Bugfix

--- custom_package_extensions/gymnasium_wrappers/state/time_limit_wrapper.py
+++ custom_package_extensions/gymnasium_wrappers/state/time_limit_wrapper.py
@@ -1,13 +1,14 @@
 from typing import SupportsFloat, Any
 from gymnasium.core import ActType, ObsType
 from gymnasium.wrappers import TimeLimit
+import gymnasium as gym
 
 from hadoop_optimizer.drl_envs.consts import ELAPSED_STEPS_KEY, MAX_STEPS_KEY
 
 
 class TimeLimitWrapper(TimeLimit):
-    def __init__(self, truncated_penalty):
-        super().__init__()
+    def __init__(self, env: gym.Env, max_episode_steps: int,  truncated_penalty: float):
+        super().__init__(env, max_episode_steps)
         self.truncated_penalty = truncated_penalty
 
     def step(
